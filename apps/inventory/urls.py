1424ddeb94cf7f2fa4be91702a9aa9c3bded507e
hamez611 <hamez611@naver.com>
2025-07-18 21:02:22 +0900

    feat: merge api 추가

--- apps/inventory/urls.py
+++ apps/inventory/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import InventoryListView, ProductVariantDetailView, InventoryItemView, ProductVariantCreateView
+from .views import InventoryListView, ProductVariantDetailView, InventoryItemView, ProductVariantCreateView, InventoryItemMergeView
 
 urlpatterns = [
     path("items/", InventoryListView.as_view(),
          name="inventory-list"),  # /api/v1/inventory/items/
+    path("items/merge/", InventoryItemMergeView.as_view(),
+         name="inventoryitem-merge"),  # api/v1/inventory/items/merge
     path("items/<int:product_id>/", InventoryItemView.as_view(),
          name="inventoryitem-detail"),  # api/v1/inventory/items/{product_id}
     path("items/<int:product_id>/variants/", ProductVariantCreateView.as_view(),
