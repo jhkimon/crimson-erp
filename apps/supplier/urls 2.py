2f517e9da9ed13e1a6b973ee0edffde7eeb4807d
jhkimon <sky06098i1@gmail.com>
2025-09-09 17:02:43 +0900

    fix: 불필요한 파일 삭제

--- apps/supplier/urls 2.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from apps.supplier.views import SupplierListCreateView, SupplierRetrieveUpdateView,  SupplierVariantUpdateView
-
-urlpatterns = [
-    path('', SupplierListCreateView.as_view(), name='supplier-list-create'),
-    path('<int:pk>/', SupplierRetrieveUpdateView.as_view(), name='supplier-detail'),
-    path('variant/<int:supplier_id>/<str:variant_code>/', SupplierVariantUpdateView.as_view(), name='supplier-variant-update')
-]
