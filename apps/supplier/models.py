319494b6f1334a180fb8eeb2a483c7f3dbfb5340
jhkimon <sky06098i1@gmail.com>
2025-11-27 14:49:58 +0900

    misc: 연락처 길이 max 수정

--- apps/supplier/models.py
+++ apps/supplier/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Supplier(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    contact = models.CharField(max_length=20)
+    contact = models.CharField(max_length=50)
     manager = models.CharField(max_length=50)
     email = models.EmailField()
     address = models.CharField(max_length=255)
