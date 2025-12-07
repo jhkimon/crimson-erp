c89cf4efa01b2199b2fd959c02201903f2c8adaa
jhkimon <sky06098i1@gmail.com>
2025-12-07 20:52:05 +0900

    chore: remove accidental duplicate files

--- apps/dashboard/urls 2.py
+++
@@ -1,7 +0,0 @@
-# apps/dashboard/urls.py
-from django.urls import path
-from .views import DashboardNotificationView
-
-urlpatterns = [
-    path('notifications/', DashboardNotificationView.as_view(), name='hr-notifications'),
-]
